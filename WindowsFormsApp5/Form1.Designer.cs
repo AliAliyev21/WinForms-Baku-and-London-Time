b93309eda0faed40a143e0ead7415248f42b0f45
GitHub Kullanıcı Adınız <GitHub E-Posta Adresini>
2023-10-04 22:25:45 +0400

    My App

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Baku";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(12, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "London";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Tai Le", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(328, 68);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(98, 44);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.BackgroundImage = global::WindowsFormsApp5.Properties.Resources.bakuimage;
+            this.ClientSize = new System.Drawing.Size(1066, 701);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        Timer timer=new Timer();
+        bool isBaku=false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackgroundImageLayout = ImageLayout.Zoom;
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            string bakuTime=DateTime.Now.ToLongTimeString();
+            string londonTime=DateTime.Now.AddHours(4).ToLongTimeString();
+            if(isBaku)
+            {
+                lblTime.Text = bakuTime;
+            }
+            else
+            {
+                lblTime.Text = londonTime;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.bakuimage;
+            isBaku = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.londonimage4;
+            isBaku = false;
+        }
+    }
+}
